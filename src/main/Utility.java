850d5b659b1a59a06c849476dd3015e7f0790b41
Sudheer narkidimilli <21pa1a1279@vishnu.edu.in>
2024-09-17 16:07:13 +0530

    SUDHEERN0001 - Check if the given number is odd or even

---
+++ src/main/Utility.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Utility{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The positive number:");
+        int num = sc.nextInt();
+        if(num %2 == 0){
+            System.out.println("The number is even");
+        }
+        else{
+            System.out.println("The number is odd");
+        }
+    }
+}
